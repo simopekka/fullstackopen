13fb0ccb5e152c9c06116102ba2e9e64c798c8c1
Simo-Pekka Varis <simopekka.varis@gmail.com>
2025-02-09 15:23:37 +0200

    removed unecessary projects and added unicafe

---
+++ part01/unicafe/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td> 
+      </tr>
+      </tbody>
+    )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100 + '%'
+  
+  if (all === 0) {
+    return (
+      <>
+        No feedback given
+      </>
+    )
+  } else {
+    return (
+      <table>
+        <StatisticLine text='good' value={good}/>
+        <StatisticLine text='neutral' value={neutral}/>
+        <StatisticLine text='bad' value={bad}/>
+        <StatisticLine text='all' value={all}/>
+        <StatisticLine text='average' value={average}/>
+        <StatisticLine text='positive' value={positive}/>
+      </table>
+    )
+  }
+}
+
+const Button = ({ type, text }) => {
+  return (
+    <button onClick={type}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => {
+    setGood(good + 1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  
+  const badClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button type={goodClick} text='good'/>
+      <Button type={neutralClick} text='neutral'/>
+      <Button type={badClick} text='bad'/>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  )
+}
+
+export default App
