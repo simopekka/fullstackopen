3af44f56b8d6cda00c64dbeb0766d9d9a71751af
Simo-Pekka Varis <simopekka.varis@gmail.com>
2025-02-11 16:25:29 +0200

    2.12.-2.15.

--- part02/puhelinluettelo/src/App.jsx
+++ part02/puhelinluettelo/src/App.jsx
@@ -1,5 +1,6 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
+import personService from './services/persons'
+
 
 const Filter = ({filter, filterInput}) => {
   return (
@@ -13,11 +14,11 @@ const Filter = ({filter, filterInput}) => {
   )
 }
 
-const PersonForm = ({addName, newName, handleNameChange, newNumber, handleNumberChange}) => {
+const PersonForm = ({addName, newName, handleInputChange}) => {
   return (
     <form onSubmit={addName}>
-      <div>name:<input value={newName} onChange={handleNameChange}/></div>
-      <div>number:<input value={newNumber} onChange={handleNumberChange}/></div>
+      <div>name:<input name="name" value={newName.name} onChange={handleInputChange}/></div>
+      <div>number:<input name="number" value={newName.number} onChange={handleInputChange}/></div>
       <div>
         <button type="submit">add</button>
       </div>
@@ -25,37 +26,40 @@ const PersonForm = ({addName, newName, handleNameChange, newNumber, handleNumber
   )
 }
 
-const Persons = ({persons, filter}) => {
+const Persons = ({persons, filter, deletePerson}) => {
   return (
     <>
       {persons.filter((person) =>
         person.name.toUpperCase().includes(filter.toUpperCase()))
         .map(person => 
-          <Person key={person.name} person={person}/>
+          <Person key={person.id} person={person} deletePerson={deletePerson}/> 
       )}
     </>
   )
 }
 
-const Person = ({person}) => {
+const Person = ({person, deletePerson}) => {
   return (
-    <p key={person.name}>{person.name} {person.number}</p>
+    <p key={person.id}>{person.name} {person.number}
+      <button onClick={() => deletePerson(person.id)}>delete</button> 
+    </p>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [newName, setNewName] = useState({
+    name: '', number: ''
+  })
   const [filter, setFilter] = useState('')
 
   const hook = () => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
+    personService
+      .getAll()
+      .then(initialPersons => {
         console.log('promise fulfilled')
-        setPersons(response.data)
+        setPersons(initialPersons)
       })
     }
 
@@ -63,32 +67,51 @@ const App = () => {
 
 
   const addName = (event) => {
-    const found = persons.find(person => person.name.toUpperCase() === newName.toUpperCase())
     event.preventDefault()
+    console.log(newName)
+    const found = persons.find(person => person.name.toUpperCase() === newName.name.toUpperCase())
+
     const personObject = {
-      name: newName,
-      number: newNumber
+      name: newName.name,
+      number: newName.number
     }
 
     if (found) {
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
-      setNewNumber('')
+      if(window.confirm(`${newName.name} is already added to the phonebook, replace the old number with a new one?`)) {
+        personService
+          .update(found.id, personObject)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== found.id ? person : returnedPerson))
+          })
+      }
     } else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+        })
+    }
+    setNewName({
+      name: '', number: ''
+    })
+  }
+
+  const deletePerson = id => {
+    console.log(id)
+    const person = persons.find(person => person.id === id)
+    const result = window.confirm(`Delete ${person.name}?`)
+    if (result) {
+      personService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
     }
   }
 
-  const handleNameChange = (event) => {
+  const handleInputChange = (event) => {
     console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
+    setNewName({...newName, [event.target.name]: event.target.value})
   }
 
   const filterInput = (event) => {
@@ -101,9 +124,9 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter filter={filter} filterInput={filterInput}/>
       <h2>Add a new</h2>
-      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+      <PersonForm addName={addName} newName={newName} handleInputChange={handleInputChange}/>
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter}/>
+      <Persons persons={persons} filter={filter} deletePerson={deletePerson}/>
     </div>
   )
 
