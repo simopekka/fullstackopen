ed01c7e2dcfcda29bc01ff270d13b8a88369b2ee
Simo-Pekka Varis <simopekka.varis@gmail.com>
2025-02-14 18:06:09 +0200

    update

--- part02/maidentiedot/src/services/countries.js
+++ part02/maidentiedot/src/services/countries.js
@@ -1,9 +1,9 @@
+import axios from "axios"
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/'
 
 const getAll = () => {
-    return fetch(`${baseUrl}api/all`)
-        .then((response) => response.json())
-        .then((data) => data);
+    const request = axios.get(`${baseUrl}api/all`)
+    return request.then(response => response.data)
 }
 
 export default { getAll }
