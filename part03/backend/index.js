a4ef8f4221056a0c93183f05ac5af4cb0fc8a1cf
Simo-Pekka Varis <simopekka.varis@gmail.com>
2025-02-18 18:50:17 +0200

    3.9.-3.11.

---
+++ part03/backend/index.js
@@ -0,0 +1,106 @@
+const http = require('http')
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const morgan = require('morgan')
+app.use(express.static('dist'))
+
+morgan.token('body', req => {
+    return JSON.stringify(req.body)
+})
+  
+app.use(morgan(':method :url :status :response-time ms :body'))
+
+const cors = require('cors')
+
+app.use(cors())
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number:"040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number:"39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number:"12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number:"39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const info = `Phonebook has info for ${persons.length} people`
+    const date = new Date()
+
+    response.send(`${info}<br>${date}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    
+    return Math.floor(Math.random() * 10000)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'Content missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+    console.log(`adding person: ${person.name} ${person.number}`)
+    const found = persons.find(person => person.name === body.name)
+    if (!found) {
+    persons = persons.concat(person)
+    response.json(person)
+    } else {
+        console.log(`${person.name} is already added to the phonebook!`)
+        return response.status(400).json({
+            error: `${person.name} is already added to the phonebook!`
+        })
+    }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log(`Server runnin on port ${PORT}`)
