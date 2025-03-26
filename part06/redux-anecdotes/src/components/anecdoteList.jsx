a97bf2f3bdcb822c444e8ff2337d82f332dc6ebe
Simo-Pekka Varis <simopekka.varis@gmail.com>
2025-03-26 13:23:37 +0200

    6.9.

--- part06/redux-anecdotes/src/components/anecdoteList.jsx
+++ part06/redux-anecdotes/src/components/anecdoteList.jsx
@@ -2,7 +2,13 @@ import { useSelector, useDispatch } from 'react-redux'
 import { newVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if (state.filter === "") {
+      return state.anecdotes
+    } else {
+      return state.anecdotes.filter((anecdote) => anecdote.content.toLowerCase().includes(state.filter))
+    }
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
